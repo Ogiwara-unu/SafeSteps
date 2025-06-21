957ec365a69354157851f35ef5aa2a9477d42f7e
Randall_Wa <randall.alvarez.ch@gmail.com>
2025-06-21 08:53:58 -0600

    last changes

--- src/models/constantes.ts
+++ src/models/constantes.ts
@@ -1,4 +1,4 @@
-export const urlbase ="http://192.168.0.8:3000/api";
+export const urlbase ="http://10.251.37.4:3000/api";
 
 export const functionUrl = `${urlbase}/manage-subscription`;
 export const ValidarCargarDeServicio = `${urlbase}/firebase-status`;
